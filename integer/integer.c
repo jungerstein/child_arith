7e33740fe19df620ce92015a9b8db28725fe4df8
jungerstein <BeatlesFan1987@gmail.com>
2015-11-02 16:22:54 +0800

    Normalise an integer.

--- integer/integer.c
+++ integer/integer.c
@@ -1,2 +1,21 @@
 /* integer.c */
 /* Routines for integer operations. */
+
+#include "integer.h"
+
+/* Count how many digits are in an int. */
+static int count_int_digit(int a)
+{
+  int n_digit = 0;
+  while (a != 0)
+  {
+    a /= 10;
+    n_digit++;
+  }
+  return n_digit;
+}
+
+static void * normalize_int(int_for_child *a)
+{
+  a->len = count_int_digit(a->val);
+}
