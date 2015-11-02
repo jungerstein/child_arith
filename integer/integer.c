7ad0c75f8635837a65cce5a36b4e0d9732be8bbd
jungerstein <BeatlesFan1987@gmail.com>
2015-11-02 16:24:43 +0800

    Add two integers

--- integer/integer.c
+++ integer/integer.c
@@ -3,10 +3,11 @@
 
 #include "integer.h"
 
-/* Count how many digits are in an int. */
+/* Count how many digits are in an int to print. */
 static int count_int_digit(int a)
 {
   int n_digit = 0;
+  if (a < 0) n_digit++;
   while (a != 0)
   {
     a /= 10;
@@ -15,7 +16,17 @@ static int count_int_digit(int a)
   return n_digit;
 }
 
-static void * normalize_int(int_for_child *a)
+static void normalize_int(int_for_child *a)
 {
   a->len = count_int_digit(a->val);
+}
+
+// Add a and b.
+int_for_child int_add(int a, int b)
+{
+  int_for_child result;
+
+  result->val = a->val + b->val;
+  normalize_int(result);
+  return result; 
 }
